ff8e1d55562aca06206a38c26c55047a3af727ff
Kamil Pawelski <kp02224@gmail.com>
2024-06-03 10:00:49 +0200

    All articles style

--- PremierLeagueBlog.Server/Data/ApiLoginRequest.cs
+++ PremierLeagueBlog.Server/Data/ApiLoginRequest.cs
@@ -2,8 +2,10 @@
 
 namespace PremierLeagueBlog.Server.Data
 {
+    // dodac validacje tu jak i w reszcie
     public class ApiLoginRequest
     {
+
         [Required]
         public required string Email { get; set; }
 
